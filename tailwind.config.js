2fb0faeb2a7a7a085b75a0d8e84ffe50390a803f
roshana01 <roshanaroshani99@gmail.com>
2023-10-25 08:54:26 +0330

    updat : file tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./*.html', './src/**/*.{html,js}'],
   theme: {
     extend: {},
+    container: {
+      padding: '2rem',
+      center: true
+    },
   },
   plugins: [],
 }
